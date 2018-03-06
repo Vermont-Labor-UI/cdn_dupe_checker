7ddcaac64e438a099a20b8267e7302f8152a302b
Chris Woerz <erendrake@gmail.com>
2018-03-06 13:31:43 -0700

    fixed args

--- dupechecker.py
+++ dupechecker.py
@@ -5,11 +5,7 @@ import requests
 
 def find_dupes(base_container_url, cdn_source_folder):
     files = []
-    for filename in glob.iglob('{0}/*'.format(cdn_source_folder), recursive=True):
-        print("\tFound file for CDN: " + filename)
-        files.append(filename)
-
-    for filename in glob.iglob('{0}/**/*'.format(cdn_source_folder), recursive=True):
+    for filename in glob.iglob('{0}/**/*.*'.format(cdn_source_folder), recursive=True):
         print("\tFound file for CDN: " + filename)
         files.append(filename)
 
@@ -24,7 +20,7 @@ def find_dupes(base_container_url, cdn_source_folder):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate an Autorest Client')
     parser.add_argument('--baseContainerUrl', type=str)
-    parser.add_argument('--cdnSourceFolder', type=int, default=1)
+    parser.add_argument('--cdnSourceFolder', type=str)
     args = parser.parse_args()
 
     dupes = find_dupes(args.baseContainerUrl, args.cdnSourceFolder)
