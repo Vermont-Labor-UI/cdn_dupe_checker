17b8046d9f23fa7d85dd67eba3531f7f764f74c8
Chris Woerz <erendrake@gmail.com>
2018-03-07 13:11:05 -0700

    fixing filenames

--- dupechecker.py
+++ dupechecker.py
@@ -11,17 +11,21 @@ def find_dupes(base_container_url, cdn_source_folder):
     search_path = './**/*.*'
     print("searching {0} for files".format(os.getcwd()))
     for filename in glob.iglob(search_path, recursive=True):
-        print("\tFound file for CDN: " + filename)
-        files.append(filename)
+        stripped_file = str(filename).replace(".\\", "")
+        print("\tFound file for CDN: " + stripped_file)
+        files.append(stripped_file)
 
     found_dupes = []
     for file in files:
         check_url = base_container_url + "/" + file
+        print()
         print("checking url {0} for existing file".format(check_url))
 
-        if requests.get(check_url).status_code == 200:
+        result = requests.get(check_url)
+        if result.status_code == 200:
             print("\tERROR: Found file already in CDN: " + file)
             found_dupes.append(file)
+
     return found_dupes
 
 
