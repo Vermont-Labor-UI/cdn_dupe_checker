e144ee496f148717aaeb7448a2ceeed502f29c55
Chris Woerz <chriswoerz@gmail.com>
2019-12-20 15:55:18 -0700

    adding build_id to dupe checking:

--- dupechecker.py
+++ dupechecker.py
@@ -4,26 +4,26 @@ import os
 import requests
 
 
-def find_dupes(base_container_url, cdn_source_folder):
+def find_dupes(base_container_url, cdn_source_folder, buildId):
     os.chdir(cdn_source_folder)
     
     files = []
     search_path = './**/*.*'
-    print("searching {0} for files".format(os.getcwd()))
+    print(f"searching {os.getcwd()} for files")
     for filename in glob.iglob(search_path, recursive=True):
         stripped_file = str(filename).replace(".\\", "")
-        print("\tFound file for CDN: " + stripped_file)
+        print(f"\tFound file for CDN: {stripped_file}")
         files.append(stripped_file)
 
     found_dupes = []
     for file in files:
-        check_url = base_container_url + "/" + file
+        check_url = f"{base_container_url}/{buildId}/{file}"
         print()
-        print("checking url {0} for existing file".format(check_url))
+        print(f"checking url {check_url} for existing file")
 
         result = requests.get(check_url)
         if result.status_code == 200:
-            print("\tERROR: Found file already in CDN: " + file)
+            print(f"\tERROR: Found file already in CDN: {file}")
             found_dupes.append(file)
 
     return found_dupes
@@ -34,8 +34,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate an Autorest Client')
     parser.add_argument('--baseContainerUrl', type=str, required=True)
     parser.add_argument('--cdnSourceFolder', type=str, required=True)
+    parser.add_argument('--buildId', type=int, required=True)
     args = parser.parse_args()
 
-    dupes = find_dupes(args.baseContainerUrl, args.cdnSourceFolder)
+    dupes = find_dupes(args.baseContainerUrl, args.cdnSourceFolder, args.buildId)
     if len(dupes) > 0:
         exit(1)
